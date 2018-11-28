8a9be8881a76517cc7ca72a8ec343f702dc8fe4b
CraftMCD123 <45150138+CraftMCD123@users.noreply.github.com>
2018-11-28 18:49:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,4 +31,4 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
  });
 
  
-client.login('NTAyOTI3NjczNjczMTIxNzky.Dt8YoA.-JOkHTRp8e3-U50vM2Jdcz3uNiQ');
+client.login('NTAxMDQ2NDgzNjY4MzAzODcy.DuBlZw.5iwxzqVBYt_WqkMjISOdYAXpgyM');
